593ac59d2772625769f54b82fe1aa630728b42f9
Jeronimo F. Martinez Sanchez <jms@rz0r.net>
2016-05-28 08:59:46 -0600

    preparing to packing and test with tox

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup 
+setup(
+    name='check-phone-number',
+    version='1.0',
+    url="https://github.com/jms/check-phone-number",
+    author="Jeronimo Martinez Sanchez",
+    author_email="jms@rz0r.net",
+    description="Bottle microservice to verify phone numbers",
+    py_modules=['main']
+)
